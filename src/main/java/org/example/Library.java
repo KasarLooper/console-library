afcc4dd1216e348eba9e612a6af09c287d253f49
KasarLooper <kasar.looper@gmail.com>
2024-06-27 20:35:39 +0800

    Add Library.java and Book.java

---
+++ src/main/java/org/example/Library.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Library {
+    public ArrayList<Book> getBooks() {
+        return new ArrayList<Book>();
+    }
+
+    public ArrayList<Book> searchBooks() {
+        return new ArrayList<>();
+    }
+
+    public void addBook(String name, String author) {
+    }
+
+    public void borrowBook(String name, String author) {
+    }
+
+    public void returnBook(String name, String author) {
+    }
+}
