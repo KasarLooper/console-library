b869836182bf2173f8f034bff670f9eec1ed1cbe
juliaVenichenko <LeoVeDm@yandex.ru>
2024-06-28 12:39:19 +0300

    Class Book: add methods getName, getAuthorName, getIsThere, setBorrowBook and constructor Book;
    Class Library:
    add methods getBooks, borrowBook and returnBook.

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,4 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Book {
+    boolean isThere;
+    String name;
+    String authorName;
+
+    Book(String name, String autorName){
+        this.name = name;
+        this.authorName = autorName;
+    }
+    String getName(){
+        return name;
+    }
+    String getAuthorName(){
+        return  authorName;
+    }
+    boolean getIsThere(){
+        return isThere = true;
+    }
+    public boolean setBorrowBook(){
+        return false;
+    }
 }
--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -3,8 +3,11 @@ package org.example;
 import java.util.ArrayList;
 
 public class Library {
+    Book book;
+    ArrayList<Book> books;
     public ArrayList<Book> getBooks() {
-        return new ArrayList<Book>();
+        return books;
+
     }
 
     public ArrayList<Book> searchBooks() {
@@ -15,8 +18,30 @@ public class Library {
     }
 
     public void borrowBook(String name, String author) {
+        for (Book book : books) {
+            if (book.getName().equals(name) && book.getAuthorName().equals(author)) {
+                if (!book.getIsThere()) {
+                book.setBorrowBook();
+                System.out.println("Книга " + name + " автора " + author + " успешно выдана.");
+            } else {
+                System.out.println("Книга " + name + " автора " + author + " занята.");
+            }
+            return;
+        }
+    }
+        System.out.println("Книги " + name + " автора " + author + " нет в библиотеке.");
     }
 
     public void returnBook(String name, String author) {
+        for (Book book : books) {
+        if (book.getName().equals(name) && book.getAuthorName().equals(author)) {
+            if (book.getIsThere()) {
+                book.setBorrowBook();
+                System.out.println("Книга " + name + " автора " + author + " возвращена.");
+            }
+            return;
+        }
+        }
+        System.out.println("Книги " + name + " автора " + author + " нет в библиотеке.");
     }
 }
